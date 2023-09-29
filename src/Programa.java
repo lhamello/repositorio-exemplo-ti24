be11d1b01ee573f829378a72be845a0bc4c11d18
a <guilhermeantonio.fuhr@gmail.com>
2023-09-29 19:29:12 -0300

    teste luis professor o melhor/  by guilherme

--- src/Programa.java
+++ src/Programa.java
@@ -1,30 +1,12 @@
-<<<<<<< HEAD
-teste professor luis 
-
-
-teste professor 
-
-
-
-luis teste professor 
-
-
-luis teste professor luis 
-
-
-
-
-
-
-
-teste professor luis 
-=======
+teste professor luis
+teste professor
+luis teste professor
+luis teste professor luis
+teste professor luis
 teste davi
 teste edson
-
-Teste Júlia Dias Ferreira 
-
+Teste Júlia Dias Ferreira
 teste luiz
 teste antonia
 teste dani S2
->>>>>>> 6df2103b3db9e0ad95908016696eba2d8af2b707
+teste luis professor o melhor/  by guilherme

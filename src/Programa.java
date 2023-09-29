718de429cfed4c97b859e9e3ad4d18532f557c09
CRISTIAN <beldoborel10@gmail.com>
2023-09-29 19:33:22 -0300

    teste cristian

--- src/Programa.java
+++ src/Programa.java
@@ -1,29 +1,18 @@
-<<<<<<< HEAD
 
 Teste Cássio
-
-
-
-
 teste dav
-
 teste henrry
-
 teste edson
 Teste Júlia Dias Ferreira
-
 Teste Júlia Dias Ferreira 
 teste Juliano
 teste luiz
-
 teste davi
 teste edson
 Teste Júlia Dias Ferreira 
 teste luiz
-
 teste antonia
 teste dani S2
-=======
 teste davi
 teste nathan
 Teste Júlia Dias Ferreira
@@ -33,15 +22,9 @@ Teste Júlia Dias Ferreira
 teste luiz
 teste edson 
 teste luiz
-
-
 teste davi
 teste edson
-=======
->>>>>>> d0f57b9ed389bfaa9d666c314cabd3092aee2244
 public class Programa {
-
-<<<<<<< HEAD
 teste juliano
 Teste Júlia Dias Ferreira
 teste davi
@@ -55,13 +38,13 @@ teste Juliano
 teste luiz
 teste antonia
 teste dani S2
-=======
+teste cristian
     public static void main(String[] args) {
         System.out.println("Hello TI24!!!");
         System.out.println("Teste!!!");        
     }
-<<<<<<< HEAD
+
 }
-=======
+
 }
->>>>>>> fbce088e5c220cba90a34d74ffed3f13e90503cb
+

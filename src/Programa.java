861ca2a3351f188ea9e00af4f353f644ddd879cb
Gabriel Ti 24 <guzzy04@hotmail.com>
2023-09-29 19:45:26 -0300

    modificado programa

--- src/Programa.java
+++ src/Programa.java
@@ -34,6 +34,7 @@ public class Programa {
 
     public static void main(String[] args) {
         System.out.println("Hello TI24!!!");
-        System.out.println("Teste!!!");        
+        System.out.println("Teste!!!");
+        System.out.println("Hello Gabriel!")
     }
 }

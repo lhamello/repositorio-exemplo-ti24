c29d53b20b395ec4421a335ebbe6f60558a660eb
Juliano <danieledjaques@yahoo.com.br>
2023-09-29 19:46:52 -0300

    Programa.java

--- src/Programa.java
+++ src/Programa.java
@@ -1,48 +1,8 @@
-<<<<<<< HEAD
-
-Teste Cássio
-
-
-
-
-teste dav
-
-teste henrry
-
-teste edson
-Teste Júlia Dias Ferreira
-
-Teste Júlia Dias Ferreira 
-teste Juliano
-teste luiz
-
-teste davi
-teste edson
-Teste Júlia Dias Ferreira 
-teste luiz
-
-teste antonia
-teste dani S2
-=======
-teste davi
-teste nathan
-Teste Júlia Dias Ferreira
-teste luiz
-teste juliano
-Teste Júlia Dias Ferreira
-teste luiz
-teste edson 
-teste luiz
-
-
-teste davi
-teste edson
-=======
->>>>>>> d0f57b9ed389bfaa9d666c314cabd3092aee2244
 public class Programa {
 
     public static void main(String[] args) {
         System.out.println("Hello TI24!!!");
-        System.out.println("Teste!!!");        
+        System.out.println("Teste!!!"); 
+        System.out.println("Juliano!!!")
     }
 }

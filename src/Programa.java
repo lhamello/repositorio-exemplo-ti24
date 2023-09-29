e0b00912fce51500a6485d4934f4f09d0c31812b
antonia <revolutionravens@proton.me>
2023-09-29 20:04:32 -0300

    antonia

--- src/Programa.java
+++ src/Programa.java
@@ -4,6 +4,6 @@ public class Programa {
     public static void main(String[] args) {
         System.out.printiln ("Hello TI24");
         System.out.println("Teste!!!");
-        System.out.println("Hello Luiz");
+        System.out.println("Hello Antonia");
     }
 }

8c3691892668def4bb084efa471a3d6740f8c9a5
Luigi Rybarcik <ederson_lima2011@hotmail.com>
2023-09-29 19:48:30 -0300

    modificado

--- src/Programa.java
+++ src/Programa.java
@@ -1,1 +1,8 @@
-teste ederson
+public class Programa {
+
+    public static void main (String[] args) {
+        System.out.println("hello TI24!!");
+        System.out.println("teste!!");
+        System.out.println("hello ederson1!");
+    }
+}

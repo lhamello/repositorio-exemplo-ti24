0c1bdd5f376fdedea304f71a76944d99885f9b30
Jean Monteiro <jean_km2009@hotmail.com>
2023-09-29 19:47:23 -0300

    Olá Jean

--- src/Programa.java
+++ src/Programa.java
@@ -2,6 +2,7 @@ public class Programa {
 
     public static void main(String[] args) {
         System.out.println("Hello TI24!!!");
-        System.out.println("Teste!!!");        
+        System.out.println("Teste!!!");
+        System.out.printl("Hello Jean!");
     }
 }

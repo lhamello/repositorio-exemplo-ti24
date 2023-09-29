28003c0c0ce2d2463904ffa33a783355e36915de
Luiz Henrique A. Mello <lhamello@gmail.com>
2023-09-29 19:45:47 -0300

    ajustado formatação

--- src/Programa.java
+++ src/Programa.java
@@ -3,6 +3,6 @@ public class Programa {
     public static void main(String[] args) {
         System.out.println("Hello TI24!!!");
         System.out.println("Teste!!!");    
-	System.out.println("Hello Luiz!");    
+	    System.out.println("Hello Luiz!");    
     }
 }

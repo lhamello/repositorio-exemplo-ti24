a77e78b2ce442de063aa0638b435912418408e08
cassio rr <casbboy@gmail.com>
2023-09-29 19:53:05 -0300

    teste ok

--- src/Programa.java
+++ src/Programa.java
@@ -40,6 +40,9 @@ public class Programa {
 
     public static void main(String[] args) {
         System.out.println("Hello TI24!!!");
-        System.out.println("Teste!!!");        
+        System.out.println("Teste!!!");
+
+	System.out.println("Hello Cássio")
+      
     }
 }

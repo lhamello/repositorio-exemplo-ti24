9d28ec695070504e52ea83ddb95bb1cd526d7f32
cassio rr <casbboy@gmail.com>
2023-09-28 22:14:37 -0300

    novo texto

--- src/Programa.java
+++ src/Programa.java
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 Teste Cássio
 
 
@@ -33,52 +32,8 @@ Teste Júlia Dias Ferreira
 teste luiz
 teste edson 
 teste luiz
-oi
-oi
-io
-ioi
-oio
-io
-io
-i
-oi
-o
-io
-i
-oi
-o
-i
-oi
-oi
-o
-io
-i
-oi
-o
-io
-io
-ioi
 
-io
-i
-oi
-oi
-o
-i
-oi
-o
-io
-i
-o
-io
-i
-o
-i
-oi
-o
-i
-oi
-o
+
 teste davi
 teste edson
 public class Programa {
